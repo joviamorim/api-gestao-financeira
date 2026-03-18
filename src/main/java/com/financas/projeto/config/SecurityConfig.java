7847f51cd921b5f0e5c47e1a3e78f0ee89256d7c
Joao Amorim <joaov.amorim02@gmail.com>
2026-03-18 20:55:51 -0300

    adding cors

--- src/main/java/com/financas/projeto/config/SecurityConfig.java
+++ src/main/java/com/financas/projeto/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(cors -> {})
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
