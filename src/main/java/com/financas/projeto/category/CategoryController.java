9bbcdb4705d9616e88959a234dc8a02c35d49aa6
Joao Amorim <joaov.amorim02@gmail.com>
2026-04-01 20:39:09 -0300

    Adicionando CategoryResponse

--- src/main/java/com/financas/projeto/category/CategoryController.java
+++ src/main/java/com/financas/projeto/category/CategoryController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.financas.projeto.category.dto.CategoryResponse;
 
 @RestController
